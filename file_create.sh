d2b764e319cb6991fc1a4facd402ea7eecbb5e81
majd <majdb173@gmail.com>
2024-12-02 22:35:41 +0200

    Adding script file

---
+++ file_create.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+file="file_create_info.txt"
+
+echo "This file explains how to create a file using touch command in linux terminal" > $file
+echo "1. Open terminal in linux" >> $file
+echo "2. Insert: touch <file_name>" >> $file
+echo "3. Go to the current dir and search for the file with the name in the previous step" >> $file
+
+echo "Thanks! hope you enjoyed!" >> $file
