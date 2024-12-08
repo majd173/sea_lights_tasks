9f6408f96261060488ba8cda66986f1a10d10425
majd <majdb173@gmail.com>
2024-12-08 20:55:14 +0200

    ci/cd updates

--- calculator/src/calculator.py
+++ calculator/src/calculator.py
@@ -4,30 +4,62 @@ class Calculator:
     """
     pass
 
-    def addition_function(self, a, b):
-        if a and b:
-            return a + b
+    @staticmethod
+    def addition_function(a, b):
+        """
+        This function manages addition process.
+        :param a:
+        :param b:
+        :return: addition result
+        """
+        if a is not None and b is not None:
+            result = a + b
+            print(f'{a} + {b} = {result}')
+            return result
         else:
             return None
 
-    def subtraction_function(self, a, b):
-        if a and b:
-            return a - b
+    @staticmethod
+    def subtraction_function(a, b):
+        """
+        This function manages addition process.
+        :param a:
+        :param b:
+        :return: subtraction result
+        """
+        if a is not None and b is not None:
+            result = a - b
+            print(f'{a} - {b} = {result}')
+            return result
         else:
             return None
 
-    def multiplication_function(self, a, b):
-        if a and b:
-            return a * b
+    @staticmethod
+    def multiplication_function(a, b):
+        """
+        This function manages multiplication process.
+        :param a:
+        :param b:
+        :return: multiplication result
+        """
+        if a is not None and b is not None:
+            result = a * b
+            print(f'{a} + {b} = {result}')
+            return result
         else:
             return None
 
-    def division_function(self, a, b):
-        try:
+    @staticmethod
+    def division_function(a, b):
+        """
+        This function manages division process.
+        :param a:
+        :param b:
+        :return: division result
+        """
+        if a is not None and b is not None:
             if b != 0:
+                result = a / b
+                print(f'{a} / {b} = {result}')
                 return a / b
-            else:
-                return "Wrong, division by zero"
-
-        except Exception as e:
-            return e
+            return "Wrong! division by zero"
---
+++ calculator/src/main.py
@@ -0,0 +1,39 @@
+from sea_lights_tasks.calculator.src.calculator import Calculator
+def main_calculator():
+    """
+    This function runs the calculator with it's all functions.
+    """
+    while True:
+        try:
+            print("Welcome to the calculator app!")
+            option = input("Please choose an option:\n1.Addition\n"
+                           "2.Subtraction\n3.Multiplication\n4.Division\n5.Exit\n")
+            if option == "1":
+                first_number = int(input("Please insert first number: "))
+                second_number = int(input("Please insert second number: "))
+                result = Calculator.addition_function(first_number, second_number)
+                print(f'{result}\n')
+            elif option == "2":
+                first_number = int(input("Please insert first number: "))
+                second_number = int(input("Please insert second number: "))
+                result = Calculator.subtraction_function(first_number, second_number)
+                print(f'{result}\n')
+            elif option == "3":
+                first_number = int(input("Please insert first number: "))
+                second_number = int(input("Please insert second number: "))
+                result = Calculator.multiplication_function(first_number, second_number)
+                print(f'{result}\n')
+            elif option == "4":
+                first_number = int(input("Please insert first number: "))
+                second_number = int(input("Please insert second number: "))
+                result = Calculator.division_function(first_number, second_number)
+                print(f'{result}\n')
+            elif option == "5":
+                print("Goodbye!")
+                exit()
+            else:
+                print("Please insert a correct option.")
+        except Exception as e:
+            print(e)
+
+main_calculator()
